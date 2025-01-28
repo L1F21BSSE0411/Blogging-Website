3f6a214d6b52400a460aa4adc4c1db4095631b5b
l1f21bsse0411 <l1f21bsse0411@ucp.edu.pk>
2025-01-29 01:49:47 +0500

    Added  config file

---
+++ configuration.sh
@@ -0,0 +1,7 @@
+git config --local user.name "l1f21bsse0411"
+git config --local user.email "l1f21bsse0411@ucp.edu.pk"
+git branch -m develop
+mkdir src styles
+touch .gitignore
+touch ./src/home.html ./styles/home.css
+npm init -y
